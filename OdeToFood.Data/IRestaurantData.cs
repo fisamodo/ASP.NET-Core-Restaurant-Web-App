3c969012729ec75d01b1850d5c72780769fbd594
fisamodo <fisamodo@vsite.hr>
2020-04-21 04:04:12 -0700

    Add project files.

---
+++ OdeToFood.Data/IRestaurantData.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using OdeToFood.Core;
+
+namespace OdeToFood.Data
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetRestaurantsByName(string name);
+    }
+
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        readonly List<Restaurant> restaurants;
+
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant { Id = 1, Name = "Scott's Pizza", Location = "Maryland", Cuisine = CuisineType.Indian },
+                new Restaurant { Id = 1, Name = "Babilo", Location = "Omis", Cuisine = CuisineType.Italian },
+                new Restaurant { Id = 1, Name = "Speddy", Location = "Omis", Cuisine = CuisineType.Mexican }
+            };
+        } 
+        public IEnumerable<Restaurant> GetRestaurantsByName(string name = null)
+        {
+            return from r in restaurants
+                   where string.IsNullOrEmpty(name) || r.Name.StartsWith(name)
+                   orderby r.Name
+                   select r;
+        }
+    }
+}
